73647abdec25020c3f4610f32646a77c68025dd0
Ashley Blurton <4752876+akblurton@users.noreply.github.com>
2023-08-03 23:58:09 +0100

    feat: ticker carousel ui

--- src/client-edit-me/edit-me/InstrumentReel.tsx
+++ src/client-edit-me/edit-me/InstrumentReel.tsx
@@ -5,10 +5,14 @@
 /**
  * ✅ You can add/edit these imports
  */
-import { Instrument, InstrumentSymbol } from "../../common-leave-me";
-import { InstrumentSocketClient } from "./InstrumentSocketClient";
+import { InstrumentSymbol } from "../../common-leave-me";
+import {
+  InstrumentSocketClient,
+  InstrumentWithChange,
+} from "./InstrumentSocketClient";
 import "./InstrumentReel.css";
-import { useEffect, useState } from "react";
+import { useEffect, useId, useState } from "react";
+import InstrumentReelItems from "./InstrumentReelItems";
 
 /**
  * ❌ Please do not edit this
@@ -22,9 +26,9 @@ function useInstruments(instrumentSymbols: InstrumentSymbol[]) {
   /**
    * ✅ You can edit inside the body of this hook
    */
-  const [instrumentData, setInstrumentData] = useState<Instrument[] | null>(
-    null
-  );
+  const [instrumentData, setInstrumentData] = useState<
+    InstrumentWithChange[] | null
+  >(null);
   const cacheKey = instrumentSymbols.join(",");
   useEffect(() => {
     const stream = client.subscribe(instrumentSymbols);
@@ -41,20 +45,50 @@ export interface InstrumentReelProps {
   instrumentSymbols: InstrumentSymbol[];
 }
 
+const MIN_ITEMS = 10;
 function InstrumentReel({ instrumentSymbols }: InstrumentReelProps) {
   /**
    * ❌ Please do not edit this
    */
   const instruments = useInstruments(instrumentSymbols);
-
   /**
    * ✅ You can edit from here down in this component.
    * Please feel free to add more components to this file or other files if you want to.
    */
 
-  return <pre>{JSON.stringify(instruments, null, 2)}</pre>;
+  // Without a unique idenifier to give to `key` at the root of this node,
+  // react's DOM manipulation messes up with flex-box in a bad way if it
+  // tries to delete a carousel off the start<->middle of the stack.
+  const id = useId();
 
-  return <div>Instrument Reel</div>;
+  // We need at least 10 items to be able to fill the carousel in a infinite way
+  // and at least two copies of the carousel to animate cleanly
+  const loops = Math.max(
+    2,
+    (instruments?.length ?? 0) > MIN_ITEMS
+      ? 1
+      : Math.ceil(MIN_ITEMS / (instruments?.length || 1))
+  );
+
+  const totalItems = instruments?.length || 0;
+
+  // I wish this were simpler, but ES3/4 array compatibility is weird
+  const iter = Array.from(Array(loops).keys());
+  return (
+    <div className="InstrumentReel" key={id}>
+      {instruments === null ? (
+        <p className="InstrumentReel__loader">Loading Instruments...</p>
+      ) : (
+        iter.map((_, index) => (
+          <InstrumentReelItems
+            total={totalItems}
+            instruments={instruments}
+            key={index}
+          />
+        ))
+      )}
+    </div>
+  );
 }
 
 export default InstrumentReel;
---
+++ src/client-edit-me/edit-me/InstrumentReelItems.tsx
@@ -0,0 +1,50 @@
+import { InstrumentWithChange } from "./InstrumentSocketClient";
+
+export interface InstrumentReelItemsProps {
+  instruments: InstrumentWithChange[];
+  total: number;
+}
+
+const DURATION_PER_ITEM = 2;
+const InstrumentReelItems = ({
+  instruments,
+  total,
+}: InstrumentReelItemsProps) => {
+  return (
+    <div
+      className="InstrumentReel__carousel"
+      style={{
+        animationDuration: `${total * DURATION_PER_ITEM}s`,
+      }}
+    >
+      {instruments.map((instrument) => (
+        <div className="InstrumentReel__item" key={instrument.code}>
+          <div className="InstrumentReel__item__icon">
+            <img
+              src={`/public/${instrument.category}/${instrument.code}.svg`}
+              className=""
+            />
+          </div>
+          <p className="Instrument__Reel__item__name">{instrument.name}</p>
+          <span
+            className={`Instrument__Reel__item__value ${
+              instrument.change > 0
+                ? "is-increase"
+                : instrument.change < 0
+                ? "is-decrease"
+                : ""
+            }`}
+          >
+            <span>{instrument.lastQuote}</span>
+            <span>
+              {instrument.change > 0 ? "+" : ""}
+              {instrument.change.toFixed(3)}%
+            </span>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default InstrumentReelItems;
